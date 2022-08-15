c55ee5642ce5c6b4b5a0361fe302d969695acf11
CiFru <rutgersucks@gmail.com>
2022-08-15 22:22:49 +0200

    Initial commit

---
+++ src/main/java/com/cifru/additionalblocks/vertical/AdditionalBlocks.java
@@ -0,0 +1,11 @@
+package com.cifru.additionalblocks.vertical;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Created 18/03/2022 by SuperMartijn642
+ */
+@Mod("abverticaledition")
+public class AdditionalBlocks {
+
+}
